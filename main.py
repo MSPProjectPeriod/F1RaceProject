7a031c6dd9672a99f8fe4261cfa2f46270a7ff56
LBrink05 <LBrinkmannx5@gmail.com>
2025-05-27 17:23:41 +0200

    added specificy for individual drivers

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import pandas as pd
 import os
 
 ### LOADING & SAVING SESSION DATA TO CVS
+csv_location = 'csv_files/'
 
 #session details
 session_year = 2024
 session_event = 'Emilia Romagna Grand Prix'
 session_type = 'R'
 session_name = session_event+'_'+str(session_year)+'_'+session_type
+session_driver = 'VER' #VERSTAPPEN
 
 # 1) Enable a cache directory (will be created if it doesn't exist)
 cache_dir = './fastf1cache'
@@ -26,26 +28,19 @@ except Exception as e:
 
 # 3) Export laps
 laps = session.laps
-laps.to_csv(session_name+'_laps.csv', index=False)
+laps.to_csv(csv_location+session_name+'_laps.csv', index=False)
 
-# 4) Export per-lap telemetry (concatenate all laps)
+# 4) Export per-lap telemetry
 # (per-lap telemetry is available via Lap.get_car_data())
-telemetry_dfs = []
-for lap in session.laps.iterlaps():
-    df = lap.get_car_data()                       # speed, throttle, brake, gear, etc.  [oai_citation:0‡GitHub](https://github.com/theOehrly/Fast-F1/issues/25?utm_source=chatgpt.com)
-    df['Driver'] = lap.driver
-    df['LapNumber'] = lap.lap_number
-    telemetry_dfs.append(df)
-if telemetry_dfs:
-    telemetry = pd.concat(telemetry_dfs, ignore_index=True)
-    telemetry.to_csv(session_name+'_telemetry.csv', index=False)
+telemetry_dfs = session.laps.pick_drivers(session_driver).get_telemetry()
+telemetry_dfs.to_csv(csv_location+session_name+'_'+session_driver+'_telemetry.csv', index=False)
 
 # 5) Export positional data
-pos_data = session.get_pos_data()                  # DataFrame of car position on track  [oai_citation:1‡Welcome to python-forum.io](https://python-forum.io/thread-40191.html?utm_source=chatgpt.com)
-pos_data.to_csv(session_name+'_posdata.csv', index=False)
+pos_data = session.laps.pick_drivers(session_driver).get_pos_data()                  # DataFrame of car position on track  [oai_citation:1‡Welcome to python-forum.io](https://python-forum.io/thread-40191.html?utm_source=chatgpt.com)
+pos_data.to_csv(csv_location+session_name+'_'+session_driver+'_posdata.csv', index=False)
 
 # 6) Export weather data
-weather = session.get_weather_data()               # WeatherData object as DataFrame  [oai_citation:2‡Welcome to python-forum.io](https://python-forum.io/thread-40191.html?utm_source=chatgpt.com)
-weather.to_csv(session_name+'_weather.csv', index=False)
+weather = session.laps.pick_drivers(session_driver).get_weather_data()               # WeatherData object as DataFrame  [oai_citation:2‡Welcome to python-forum.io](https://python-forum.io/thread-40191.html?utm_source=chatgpt.com)
+weather.to_csv(csv_location+session_name+'_'+session_driver+'_weather.csv', index=False)
 
 print("All session data exported to CSV!")
