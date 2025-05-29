c204101e0ac5e9bd623af95e20a2d31c6140ec3a
LBrink05 <LBrinkmannx5@gmail.com>
2025-05-29 09:49:54 +0200

    made csv files delete if unnecessary

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import fastf1
 import numpy as np
 import pandas as pd
 import os
-import scipy
 import matplotlib.pyplot as plt
 
 ### LOADING & SAVING SESSION DATA TO CVS
@@ -16,12 +15,12 @@ session_type = 'R'
 session_name = session_event+'_'+str(session_year)+'_'+session_type
 session_driver = 'VER' #VERSTAPPEN
 
-# 1) Enable a cache directory (will be created if it doesn't exist)
+# Enable a cache directory (will be created if it doesn't exist)
 cache_dir = './fastf1cache'
 os.makedirs(cache_dir, exist_ok=True)
 fastf1.Cache.enable_cache(cache_dir)
 
-# 2) Load the session with everything
+# Load the session with everything
 try:
     session = fastf1.get_session(session_year, session_event, session_type)
     session.load(telemetry=True, laps=True, weather=True)
@@ -31,26 +30,31 @@ except Exception as e:
 
 print("Succesfully loaded session! \n")
 
-'''
-# 3) Export laps
-laps = session.laps
-laps.to_csv(csv_location+session_name+'_laps.csv', index=False)
 
-# 4) Export per-lap telemetry
+# Export laps
+#removing unnecessary files
+for csv_file in os.listdir(csv_location):
+    if csv_file != (session_name+'_laps.csv') and csv_file != (session_name+'_'+session_driver+'_posdata.csv') and csv_file != (session_name+'_'+session_driver+'_telemetry.csv') and csv_file != (session_name+'_'+session_driver+'_weather.csv'):
+        os.remove(csv_location+csv_file)
+print("Removed all prior csv_files that were unnecessary. \n")
+
+session.laps.to_csv(csv_location+session_name+'_laps.csv', index=False)
+
+# Export per-lap telemetry
 # (per-lap telemetry is available via Lap.get_car_data())
 telemetry_dfs = session.laps.pick_drivers(session_driver).get_telemetry()
 telemetry_dfs.to_csv(csv_location+session_name+'_'+session_driver+'_telemetry.csv', index=False)
 
-# 5) Export positional data
+# Export positional data
 pos_data = session.laps.pick_drivers(session_driver).get_pos_data()                  # DataFrame of car position on track  [oai_citation:1‡Welcome to python-forum.io](https://python-forum.io/thread-40191.html?utm_source=chatgpt.com)
 pos_data.to_csv(csv_location+session_name+'_'+session_driver+'_posdata.csv', index=False)
 
-# 6) Export weather data
+# Export weather data
 weather = session.laps.pick_drivers(session_driver).get_weather_data()               # WeatherData object as DataFrame  [oai_citation:2‡Welcome to python-forum.io](https://python-forum.io/thread-40191.html?utm_source=chatgpt.com)
 weather.to_csv(csv_location+session_name+'_'+session_driver+'_weather.csv', index=False)
 
 print("All session data exported to CSV!")
-'''
+
 
 #functions
 #work on this tomorrow
